283599687bd18171621bae31753fd95a6457a0b9
Daniel <tejadadaniel33@gmail.com>
2021-01-23 21:34:32 -0400

    project structure

---
+++ EmploymentApp.Api/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmploymentApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Welcome()
+        {
+            return Ok("Welcome");
+        }
+    }
+}
